13e5e8935d63602693b9cb178ced6eb83c2af066
santi <santiagolosa95@gmail.com>
2023-06-12 22:09:16 -0300

    added some hardocded events and app name

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -35,7 +35,7 @@ export default {
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="">NOMBRE APP</a>
+            <a class="navbar-brand" href="">BICICLETAPP</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
--- frontend/src/components/Calendar/Calendar.vue
+++ frontend/src/components/Calendar/Calendar.vue
@@ -9,17 +9,41 @@ export default {
 		return {
             events: [
                 {
-                    start: '2023-06-12 07:00', // Required.
-                    end: '2023-06-12 19:00', // Required.
-                    title: 'Entrenamiento 1', // Optional.
-                    class: 'sport'
+                    start: '2023-06-12 10:00', // Required.
+                    end: '2023-06-12 12:00', // Required.
+                    title: 'Entrenamiento de Fuerza', // Optional.
+                    class: 'strength'
                 },
                 {
-                    start: new Date(2023, 6 - 1, 14, 12, 0), // Required.
-                    end: new Date(2023, 6 - 1, 14, 20, 0), // Required.
-                    title: "hola evento", // Optional.
-                    class: 'sport'
-                }
+                    start: '2023-06-13 11:00', // Required.
+                    end: '2023-06-13 13:0', // Required.
+                    title: 'Entrenamiento de Resistencia', // Optional.
+                    class: 'resistance'
+                },
+                {
+                    start: '2023-06-14 08:30', // Required.
+                    end: '2023-06-14 11:30', // Required.
+                    title: 'Entrenamiento de Velocidad', // Optional.
+                    class: 'speed'
+                },
+                {
+                    start: '2023-06-15 08:00', // Required.
+                    end: '2023-06-15 14:00', // Required.
+                    title: 'Entrenamiento de Cadencia', // Optional.
+                    class: 'cadency'
+                },
+                {
+                    start: '2023-06-17 08:00', // Required.
+                    end: '2023-06-17 16:00', // Required.
+                    title: 'Entrenamiento de resistencia', // Optional.
+                    class: 'resistance'
+                },
+                {
+                    start: '2023-06-18 07:00', // Required.
+                    end: '2023-06-18 13:00', // Required.
+                    title: 'Entrenamiento de velocidad', // Optional.
+                    class: 'speed'
+                },
             ]
         }
     },
@@ -53,6 +77,8 @@ export default {
 </template>
 <style>
 /* Different color for different event types. */
-.vuecal__event.leisure {background-color: rgba(253, 156, 66, 0.9);border: 1px solid rgb(233, 136, 46);color: #fff;}
-.vuecal__event.sport {background-color: rgba(255, 102, 102, 0.9);border: 1px solid rgb(235, 82, 82);color: #fff;}
+.vuecal__event.speed {background-color: rgba(13, 110, 253);border: 1px solid rgb(233, 136, 46);color: #fff;}
+.vuecal__event.strength {background-color: rgba(220, 53, 69);border: 1px solid rgb(235, 82, 82);color: #fff;}
+.vuecal__event.cadency {background-color: rgba(25, 135, 84);border: 1px solid rgb(235, 82, 82);color: #fff;}
+.vuecal__event.resistance {background-color: rgba(255, 193, 7);border: 1px solid rgb(235, 82, 82);color: #fff;}
 </style>
