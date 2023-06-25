33ff62299afb3b010086b9c2600ae997d981f814
santi <santiagolosa95@gmail.com>
2023-06-25 19:44:24 -0300

    add icons to calendar and added container to it

--- frontend/src/components/Calendar/Calendar.vue
+++ frontend/src/components/Calendar/Calendar.vue
@@ -11,37 +11,37 @@ export default {
                 {
                     start: '2023-06-12 10:00', // Required.
                     end: '2023-06-12 12:00', // Required.
-                    title: 'Entrenamiento de Fuerza', // Optional.
+                    title: '<i class="icon material-icons">fitness_center</i> Entrenamiento de Fuerza', // Optional.
                     class: 'strength'
                 },
                 {
                     start: '2023-06-13 11:00', // Required.
                     end: '2023-06-13 13:0', // Required.
-                    title: 'Entrenamiento de Resistencia', // Optional.
+                    title: '<i class="icon material-icons">directions_run</i> Entrenamiento de Resistencia', // Optional.
                     class: 'resistance'
                 },
                 {
                     start: '2023-06-14 08:30', // Required.
                     end: '2023-06-14 11:30', // Required.
-                    title: 'Entrenamiento de Velocidad', // Optional.
+                    title: '<i class="icon material-icons">bolt</i> Entrenamiento de Velocidad', // Optional.
                     class: 'speed'
                 },
                 {
                     start: '2023-06-15 08:00', // Required.
                     end: '2023-06-15 14:00', // Required.
-                    title: 'Entrenamiento de Cadencia', // Optional.
+                    title: '<i class="icon material-icons">timer</i> Entrenamiento de Cadencia', // Optional.
                     class: 'cadency'
                 },
                 {
                     start: '2023-06-17 08:00', // Required.
                     end: '2023-06-17 16:00', // Required.
-                    title: 'Entrenamiento de resistencia', // Optional.
+                    title: '<i class="icon material-icons">directions_run</i> Entrenamiento de resistencia', // Optional.
                     class: 'resistance'
                 },
                 {
                     start: '2023-06-18 07:00', // Required.
                     end: '2023-06-18 13:00', // Required.
-                    title: 'Entrenamiento de velocidad', // Optional.
+                    title: '<i class="icon material-icons">bolt</i> Entrenamiento de velocidad', // Optional.
                     class: 'speed'
                 },
             ]
@@ -69,12 +69,14 @@ export default {
 }
 </script>
 <template>
-    <vue-cal
-    locale="es"
-    :disable-views="['day']"
-    :events="events"
-    :on-event-click="onEventClick"
-    @cell-dblclick="toTrainingForm($event)" />
+    <div class="container">
+        <vue-cal
+        locale="es"
+        :disable-views="['day']"
+        :events="events"
+        :on-event-click="onEventClick"
+        @cell-dblclick="toTrainingForm($event)" />
+    </div>
 </template>
 <style>
 /* Different color for different event types. */
