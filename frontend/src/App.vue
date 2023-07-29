78d7cb5e3682bb6140725abf1d6c200606a8966d
SantiLosa <74719492+SantiLosa@users.noreply.github.com>
2023-07-29 16:05:58 -0300

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -65,7 +65,7 @@ export default {
 <template>
   <nav v-if="loggedIn" class="navbar navbar-expand-lg navbar-dark bg-dark"> <!--este v-if esconde la nav-bar si no estas loggeado-->
         <div class="container">
-            <a class="navbar-brand" href="#/home">NOMBRE APP</a>
+            <a class="navbar-brand" href="#/home">BiciApp</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
