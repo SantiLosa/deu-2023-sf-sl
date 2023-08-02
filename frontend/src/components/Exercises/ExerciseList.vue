ade707054fbef0dd242a68fb097604d971c431b5
santi <santiagolosa95@gmail.com>
2023-08-01 23:10:29 -0300

    extracted exercises list to file

--- frontend/src/components/Exercises/ExerciseList.vue
+++ frontend/src/components/Exercises/ExerciseList.vue
@@ -36,6 +36,7 @@
   </template>
 
   <script>
+  import presetExercises from '../exerciseData.js';
   export default {
     name: 'ExerciseList',
     props: {
@@ -43,149 +44,7 @@
     },
     data() {
       return {
-        presetExercises: [
-          { 
-            id: 1,
-            exerciseType: 'Velocidad',
-            name: 'Velocidad Ligero',
-            description: 'Entrenamiento enfocado en velocidad para principiantes',
-            duration: 60 //minutos
-          },
-          {
-            id: 2,
-            exerciseType: 'Resistencia',
-            name: 'Resistencia Ligero',
-            description: 'Entrenamiento enfocado en resistencia para principiantes',
-            duration: 90 //minutos
-          },
-          {
-            id: 3,
-            exerciseType: 'Cadencia',
-            name: 'Cadencia Ligero',
-            description: 'Entrenamiento enfocado en cadencia para principiantes',
-            duration: 60 //minutos
-          },
-          {
-            id: 4,
-            exerciseType: 'Fuerza',
-            name: 'Fuerza Ligero',
-            description: 'Entrenamiento enfocado en fuerza para principiantes',
-            duration: 30 //minutos
-          },
-          {
-            id: 5,
-            exerciseType: 'Fuerza',
-            name: 'Fuerza Intenso',
-            description: 'Entrenamiento colina arriba para practicar fuerza. No apto para principiantes',
-            duration: 45
-          },
-          {
-            id: 6,
-            exerciseType: 'Fuerza',
-            name: 'Fuerza Intenso con Pausa',
-            description: 'Similar a Fuerza Intenso pero con pausas mas largas entre segmentos',
-            duration: 65
-          },
-          {
-            id: 7,
-            exerciseType: 'Resistencia',
-            name: 'Tour de france',
-            description: 'Entrenamiento extremo de aguante',
-            duration: 430
-          },
-          {
-            id: 8,
-            exerciseType: 'Resistencia',
-            name: 'Vuelta al mundo',
-            description: 'Recorrido por todo el cuadrado de la plata',
-            duration: 300
-          },
-          {
-            id: 9,
-            exerciseType: 'Velocidad',
-            name: 'Carrera alterna',
-            description: 'Camino por el bosque a maxima velocidad',
-            duration: 40
-          },
-          {
-            id: 10,
-            exerciseType: 'Cadencia',
-            name: 'Bici fija rpm',
-            description: '20 minutos de calentamiento, ejercicio, 20 mins para bajar.',
-            duration: 180
-          },
-          {
-            id: 11,
-            exerciseType: 'Fuerza',
-            name: 'Bici en cambio duro',
-            description: '10 minutos de calentamiento en configuracion tranqui, luego cambio a duro sin parar',
-            duration: 40
-          },
-          {
-            id: 12,
-            exerciseType: 'Velocidad',
-            name: 'Flash special',
-            description: 'Ejercicio para intentar empujar la velocidad maxima del ciclista',
-            duration: 90
-          },
-          {
-            id: 13,
-            exerciseType: 'Resistencia',
-            name: 'Aguante en alta cadencia',
-            description: 'Mantener una cadencia decente durante mucho tiempo en bici fija, con pausas',
-            duration: 180
-          },
-          {
-            id: 14,
-            exerciseType: 'Fuerza',
-            name: 'Resistencia',
-            description: 'Resistencia mediano',
-            duration: 30
-          },
-          {
-            id: 15,
-            exerciseType: 'Cadencia',
-            name: 'Calentamiento',
-            description: 'Unos minutos de cadencia media para calentar',
-            duration: 20
-          },
-          {
-            id: 16,
-            exerciseType: 'Fuerza',
-            name: 'Bici dura con pausas',
-            description: 'Alternar entre cadencia baja con bici dura y cadencia alta con bici floja.',
-            duration: 80
-          },
-          {
-            id: 17,
-            exerciseType: 'Fuerza',
-            name: 'Michelin special',
-            description: 'En bicicleta con rueda atada atras. ',
-            duration: 60
-          },
-          {
-            id: 18,
-            exerciseType: 'Velocidad',
-            name: 'Cardio basico',
-            description: 'Ejercicio de velocidad alta para entrenar cardio',
-            duration: 55
-          },
-          {
-            id: 19,
-            exerciseType: 'Velocidad',
-            name: 'Burst',
-            description: 'Ejercicio de aceleracion, estrechos cortos de alta velocidad',
-            duration: 40
-          },
-          {
-            id: 20,
-            exerciseType: 'Velocidad',
-            name: 'Burst intenso',
-            description: 'Muchos estrechos cortos de alta velocidad',
-            duration: 100
-          },
-          // Add more preset exercises as needed...
-        ],
+        presetExercises: presetExercises,
         searchQuery: '',
       }
     },
